fea72ab9601c86ea7248a4d5af9f5a5ff078e181
Mathias Forsman <gusmathfo@student.gu.se>
2020-10-09 13:05:03 +0200

    fixed issues with files to setup the project

---
+++ JazzCode/app/src/main/java/com/example/nfinderc/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.nfinderc
+
+import android.content.Context
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        }
+}
