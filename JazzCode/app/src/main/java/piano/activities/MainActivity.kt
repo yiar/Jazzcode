94aab0ee1e1f2c38cdcf6c913f3a528ac4870406
Mathias Forsman <gusmathfo@student.gu.se>
2020-10-17 20:15:40 +0200

    set up code to swap between activities, safe push to go back to

--- JazzCode/app/src/main/java/piano/activities/MainActivity.kt
+++ JazzCode/app/src/main/java/piano/activities/MainActivity.kt
@@ -1,13 +1,24 @@
 package piano.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
+import android.widget.Button
 import com.example.nfinderc.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ha en logik, om inte inloggad, visa login activity
+
+        val logBtn = findViewById<Button>(R.id.loggain)
+        logBtn.setOnClickListener {
+            // Handler code here.
+            val intent = Intent(this, logInActivity::class.java)
+            startActivity(intent);
         }
+        }
+
 }
