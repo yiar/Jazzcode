77e37491fcdf7a2b4fc445bff48d2ebb9af63b90
Mathias Forsman <gusmathfo@student.gu.se>
2020-10-23 16:30:09 +0200

    fixed activities

--- JazzCode/app/src/main/java/piano/activities/MainActivity.kt
+++ JazzCode/app/src/main/java/piano/activities/MainActivity.kt
@@ -8,8 +8,6 @@ import android.widget.Button
 import com.example.nfinderc.R
 import piano.classes.DiffList
 
-// listOf<String>("C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" )
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,12 +15,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        //if(handler.keysList.isEmpty()) {
-         //   setContentView(R.layout.activity_resetpw)
-      //  }
-        //else {
-            setContentView(R.layout.activity_main)
 
 
         val logBtn = findViewById<Button>(R.id.loggain)
@@ -120,24 +114,3 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-
-// Piano Keys
-// Confirm password button (under piano)
-// Clear input (to right)
-// Visually show how many keys you've pressed compared to full length of password/melody (maybe)
-/* <View
-android:id="@+id/group_55"
-android:layout_width="116dp"
-android:layout_height="46dp"
-android:layout_alignParentLeft="true"
-android:layout_marginLeft="122dp"
-android:layout_alignParentTop="true"
-android:layout_marginTop="207dp"
-android:background="#00000000"
-/>
- */
-
-
-
-
-// Show which key is the startNote. Color the note, or just write enum as text above piano.
